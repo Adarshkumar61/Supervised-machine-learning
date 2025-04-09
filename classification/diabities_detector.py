a8c117aab0ad6bdbc32958e3dd188595b20cee61
Adarshkumar61 <adarshb2k61@gmail.com>
2025-04-09 20:40:22 +0530

    supervised learning

---
+++ classification/diabities_detector.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('C:\Users\adars\Adarsh\data.csv')
+
+x = data.drop(columns= 'outcome', axis= 1)
+y = data['outcome']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, stratify= y, random_state=2)
+
+linear_model = LogisticRegression()
+
+linear_model.fit(x_train, y_train)
+
+predict = linear_model.predict(x_train)
+accurcy_of_x_train = accuracy_score(x_train, predict)
+
+#creating a input parameter:
+
+user_input = ()
+in_numpy = np.asarray(user_input)
+
+reshaped_model = in_numpy.reshape(1, -1)
+print(reshaped_model)
