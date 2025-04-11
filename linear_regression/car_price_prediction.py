329fb443bd337c2c3327666e4d62b834cfa7e682
Adarshkumar61 <adarshb2k61@gmail.com>
2025-04-11 22:53:10 +0530

    updated code

--- linear_regression/car_price_prediction.py
+++ linear_regression/car_price_prediction.py
@@ -38,7 +38,7 @@ y = data['Selling_Price']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, random_state=2)
 
 #now we will call our model:
- 
+
 model = LinearRegression()
 
 model.fit(x_train, y_train)
