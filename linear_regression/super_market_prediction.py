b6c08caff2b06f96a3e5be724c55604004509ee6
Adarshkumar61 <adarshb2k61@gmail.com>
2025-04-13 14:20:23 +0530

    creating a mall sales predictor model

---
+++ linear_regression/super_market_prediction.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+#loading the dataset:
