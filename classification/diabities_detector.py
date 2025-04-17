6b3f863c5f89f114c24879ab78d96af9f0bed68f
Adarshkumar61 <adarshb2k61@gmail.com>
2025-04-17 08:54:39 +0530

    updated code

--- classification/diabities_detector.py
+++ classification/diabities_detector.py
@@ -7,17 +7,33 @@ from sklearn.metrics import accuracy_score
 
 data = pd.read_csv('C:\Users\adars\Adarsh\data.csv')
 
-x = data.drop(columns= 'outcome', axis= 1)
-y = data['outcome']
+#check if there is any missing value present or not:
+data.isnull().sum()
 
+# we are gonna seperate feature and column: 
+
+x = data.drop(columns= 'outcome', axis= 1) #feature
+y = data['outcome']  #target
+
+#splitting data into training and testing test: 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, stratify= y, random_state=2)
 
+# now we call our model:
 linear_model = LogisticRegression() #our model
 
 linear_model.fit(x_train, y_train) #training the data
 
+#prediction on x_train and x_test:
+
 predict = linear_model.predict(x_train)
-accurcy_of_x_train = accuracy_score(x_train, predict)
+predict_x_test = linear_model.predict(x_test) #now we will check the accuracy of our model:
+
+# on x_train:
+accuracy_of_x_train = accuracy_score(y_train, predict)
+
+#on x_test:
+accuracy_of_x_test = accuracy_score(y_test, predict_x_test)
+
 
 #creating a input parameter:
 
