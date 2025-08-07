e0c98a26b4407d423f99d025d63228df4e55690a
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-07 19:10:56 +0530

    updated

--- linear_regression/model_using_PipleLine_with_preprocessing_On_custom_DataSet.py
+++ linear_regression/model_using_PipleLine_with_preprocessing_On_custom_DataSet.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
 
 
 data = {
