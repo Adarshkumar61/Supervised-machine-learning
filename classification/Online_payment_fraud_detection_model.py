6527d6d77d96e3ca4817caefae4ff6346118dba8
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-28 21:58:43 +0530

    model created and predicted properly;

--- classification/Online_payment_fraud_detection_model.py
+++ classification/Online_payment_fraud_detection_model.py
@@ -20,3 +20,37 @@ print(data.head())
 # 3    C38997010         21182.0             0.0        1               0  
 # 4  M1230701703             0.0             0.0        0               0  
 
+data.isnull().sum()
+# step              0
+# type              0
+# amount            0
+# nameOrig          0
+# oldbalanceOrg     0
+# newbalanceOrig    0
+# nameDest          0
+# oldbalanceDest    0
+# newbalanceDest    0
+# isFraud           0
+# isFlaggedFraud    0
+
+# transforming categorical data to numerical:
+
+data['type'] = data['type'].map({'CASH_OUT': 0, 'PAYMENT': 1, 'CASH_IN': 2, 'TRANSFER': 3, 'DEBIT': 4})
+
+data['isFraud'] = data['isFraud'].map({0: "No Fraud", 1: "Fraud"})
+
+x = data['type', 'amount', 'oldbalanceOrg', 'newbalanceOrig']
+y = data['isFraud']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, random_state= 42)
+
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+
+print(model.score(x_test, y_test))
+# 0.9995
+
+#predicting a new data point:
+#features = [type, amount, oldbalancerg, newbalancerig]
+new_data = np.array([[0, 181.00, 181.0, 0.00]])
+prediction = model.predict(new_data)
