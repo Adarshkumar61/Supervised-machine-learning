3841a2938319662596e495db530efe7f181dc148
Adarshkumar61 <adarshb2k61@gmail.com>
2025-09-02 08:48:39 +0530

    updated

--- linear_regression/Future_Sales_prediction.py
+++ linear_regression/Future_Sales_prediction.py
@@ -13,3 +13,13 @@ print(data.head())
 # 3  151.5   41.3       58.5   16.5
 # 4  180.8   10.8       58.4   17.9
 
+data.isnull().sum()
+# TV           0
+# Radio        0
+# Newspaper    0
+# Sales        0
+# so there is no missing values
+
+x = data.drop('Sales', axis= 1) # features
+y = data['Sales'] # target
+
