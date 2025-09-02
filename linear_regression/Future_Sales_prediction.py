bc6b9e1d0f1c0230b1bbbd5a3cfce553d436869b
Adarshkumar61 <adarshb2k61@gmail.com>
2025-09-02 08:47:39 +0530

    removed mae

--- linear_regression/Future_Sales_prediction.py
+++ linear_regression/Future_Sales_prediction.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.metrics import r2_score
 
